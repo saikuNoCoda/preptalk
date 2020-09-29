f187d30912562930cf8667486f902fcbc4d29391
failedcoder12 <divyanshverma12@gmail.com>
2020-09-29 13:22:17 +0530

    update

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -6,29 +6,10 @@ const teamData = [
         linkLink: "divyanshvermafc",
         linkFace: "failedcoder",
         offers: [
+            "Myntra",
             "Samsung"
         ]
     },
-    {
-        id: 2,
-        name: "Divyansh Verma",
-        profile: "Software Engineer",
-        linkLink: "divyanshvermafc",
-        linkFace: "failedcoder",
-        offers: [
-            "Samsung"
-        ]
-    }, 
-    {
-        id: 3,
-        name: "Divyansh Verma",
-        profile: "Software Engineer",
-        linkLink: "divyanshvermafc",
-        linkFace: "failedcoder",
-        offers: [
-            "Samsung"
-        ]
-    },  
 ];
 
 export default teamData;
--- src/data/userData.js
+++ src/data/userData.js
@@ -7,7 +7,6 @@ const userData = [
         description: "I became more confindent in Interview. It also helps me to improve my problem solving skills we had an in depth discussion on my projects and resume, which gave me an idea of the kind of questions which can be asked in a real interview. The post interview feedback was very insightful as I was given tips on how I can improve both my technical as well as communication skills. ",
         offers: [
             "Citi India",
-            "Make My Trip"
         ]
     },
     {
@@ -27,10 +26,8 @@ const userData = [
         link: "vichitrgandas",
         description: "I became more confindent in Interview. It also helps me to improve my problem solving skills we had an in depth discussion on my projects and resume, which gave me an idea of the kind of questions which can be asked in a real interview. The post interview feedback was very insightful as I was given tips on how I can improve both my technical as well as communication skills. ",
         offers: [
-            "Google",
             "Visa",
-            "Samsung",
-            "Bloomberg"
+            "Samsung"
         ]
     }
 ];
