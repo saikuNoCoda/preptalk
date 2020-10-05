f26ee44b90d0267b3f1b03c64a28edd3520fd5af
failedcoder12 <divyanshverma12@gmail.com>
2020-10-05 16:38:50 +0530

    update1

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -3,8 +3,8 @@ const teamData = [
         id: 1,
         name: "Divyansh Verma",
         profile: "Software Engineer",
-        linkLink: "https://www.linkedin.com/in/divyanshvermafc/",
-        linkFace: "https://www.facebook.com/failedcoder/",
+        linkLink: "divyanshvermafc",
+        linkFace: "failedcoder",
         offers: [
             "Myntra",
             "Samsung R&D"
@@ -14,8 +14,8 @@ const teamData = [
         id: 2,
         name: "Vichitr",
         profile: "Software Engineer",
-        linkLink: "https://www.linkedin.com/in/vichitrgandas/",
-        linkFace: "https://www.facebook.com/vichitrgandas/",
+        linkLink: "vichitrgandas",
+        linkFace: "vichitrgandas",
         offers: [
             "Myntra",
             "Samsung Semiconductor"
@@ -25,8 +25,8 @@ const teamData = [
         id: 3,
         name: "Anmol Horo",
         profile: "Software Engineer",
-        linkLink: "https://www.linkedin.com/in/anmolh123/",
-        linkFace: "https://www.facebook.com/anmol.horo.90",
+        linkLink: "anmolh123",
+        linkFace: "anmol.horo.90",
         offers: [
             "Make My Trip",
         ]
--- src/data/userData.js
+++ src/data/userData.js
@@ -1,8 +1,18 @@
 const userData = [
     {
         id: 1,
+        name: "Adarsh kumar",
+        college: "National institute of technology Karnataka",
+        link: "adarsh-sreyan",
+        description: "First thing it give me confidence to clear my interview. Before joining this I know the things which is asked in interview but I don't know how to explain the things after joining this I give a session after that  they pick my error in how I give interview like first approach Brut Force approach then optimize approach and pick standard words for explaining the things like if we want to use count of a number then use word hashing etc. After giving all these mock interview I understand my mistake and work on it and I finally placed in Amdocs. Thank you 😊",
+        offers: [
+            "Amdocs"
+        ]
+    },
+    {
+        id: 2,
         name: "Rupesh Nitnaware",
-        college: "NIT Karnataka",
+        college: "National institute of technology Karnataka",
         link: "imgr00t",
         description: "I became more confindent in Interview. It also helps me to improve my problem solving skills we had an in depth discussion on my projects and resume, which gave me an idea of the kind of questions which can be asked in a real interview. The post interview feedback was very insightful as I was given tips on how I can improve both my technical as well as communication skills. ",
         offers: [
