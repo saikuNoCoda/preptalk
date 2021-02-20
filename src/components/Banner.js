dad99cb2a54a06398f1e0ea8812dbdd2061bfe3b
Divyansh Verma <divyanshverma12@gmail.com>
2021-02-20 19:40:50 +0530

    bug resolved

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export default class Banner extends Component {
         
             <Col sm={4} className="text-center text-md-left mt-5 mx-4">
                 <h1>Here Comes the PrepTalk!!!</h1>
-                <h4 className="text-muted mt-4">The best and most efficent way to prepare for your upcoming technical interviews.
+                <h4 className="text-muted mt-4">The best and most efficient way to prepare for your upcoming technical interviews.
                 Helps you prepare for every level of technical interviews</h4>
                 <a href="#contact">
                 <Button variant="dark" className="my-4">Try It Out</Button>
