fe0b4b2816addb29d167cb4f757c98efc3507d98
Divyansh Verma <divyanshverma12@gmail.com>
2021-02-20 20:58:01 +0530

    bug

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     return (
             <div className="text-center p-5 bg-light">
                 <h1>For any other query contact us @ </h1>
-                <h3 className="text-muted">Prepare the best way for your incoming interviews <a href="mailto:preptalks1@gmail.com">preptalk1@gmail.com</a></h3>
+                <h3 className="text-muted">Prepare the best way for your incoming interviews <a href="mailto:preptalks1@gmail.com">preptalks1@gmail.com</a></h3>
             </div>
     )
 };
